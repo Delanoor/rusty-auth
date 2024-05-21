796c5443f00c2c823dac601629fdedfdab8e5298
hwn0809 <hwn0809@gmail.com>
2024-05-21 13:17:41 +0900

    add AppState & signup route

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,5 @@
 use crate::helpers::{get_random_email, TestApp};
+use auth_service::routes::SignupResponse;
 
 #[tokio::test]
 async fn should_return_422_if_malformed_input() {
@@ -35,4 +36,31 @@ async fn should_return_422_if_malformed_input() {
             test_case
         );
     }
+}
+
+#[tokio::test]
+async fn should_return_201_if_valid_input() {
+    let app = TestApp::new().await;
+    let body = serde_json::json!(
+        {
+            "email": "test_user@email.com",
+            "password": "12341234",
+            "requires2FA": true
+        }
+    );
+    let response = app.post_signup(&body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    let expected_response = SignupResponse {
+        message: "User created successfully!".to_owned(),
+    };
+
+    assert_eq!(
+        response
+            .json::<SignupResponse>()
+            .await
+            .expect("Could not deserialize response body to UserBody"),
+        expected_response
+    );
 }
