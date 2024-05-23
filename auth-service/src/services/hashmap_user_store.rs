744ffb6ff75f64aa3eb029f284283be975acb60c
hwn0809 <hwn0809@gmail.com>
2024-05-23 14:31:21 +0900

    Add Email & Password types

--- auth-service/src/services/hashmap_user_store.rs
+++ auth-service/src/services/hashmap_user_store.rs
@@ -2,11 +2,11 @@ use std::collections::HashMap;
 
 use crate::domain::{
     data_stores::{UserStore, UserStoreError},
-    user::User,
+    user::{Email, Password, User},
 };
 
 pub struct HashmapUserStore {
-    users: HashMap<String, User>,
+    users: HashMap<Email, User>,
 }
 
 impl Default for HashmapUserStore {
@@ -16,10 +16,10 @@ impl Default for HashmapUserStore {
         };
 
         user_store.users.insert(
-            "admin@email.com".to_string(),
+            Email("admin@email.com".to_string()),
             User {
-                email: "admin@email.com".to_string(),
-                password: "123123".to_string(),
+                email: Email("admin@email.com".to_string()),
+                password: Password("123123".to_string()),
                 requires_2fa: false,
             },
         );
@@ -29,25 +29,25 @@ impl Default for HashmapUserStore {
 
 #[async_trait::async_trait]
 impl UserStore for HashmapUserStore {
-    async fn add_user(&mut self, user: User) -> Result<(), UserStoreError> {
+    async fn add_user(&mut self, user: &User) -> Result<(), UserStoreError> {
         match self.users.get(&user.email) {
             Some(_) => Err(UserStoreError::UserAlreadyExists),
             None => {
-                self.users.insert(user.email.to_string(), user);
+                self.users.insert(user.email.clone(), user.clone());
                 Ok(())
             }
         }
     }
 
-    async fn get_user(&self, email: &str) -> Result<User, UserStoreError> {
+    async fn get_user(&self, email: &Email) -> Result<User, UserStoreError> {
         match self.users.get(email) {
             Some(user) => Ok(user.clone()),
             None => Err(UserStoreError::UserNotFound),
         }
     }
 
-    async fn validate_user(&self, email: &str, password: &str) -> Result<(), UserStoreError> {
-        match self.users.get(email) {
+    async fn validate_user(&self, email: Email, password: Password) -> Result<(), UserStoreError> {
+        match self.users.get(&email) {
             Some(user) => {
                 if user.password != password {
                     Err(UserStoreError::InvalidCredentials)
@@ -68,13 +68,13 @@ mod tests {
     async fn test_add_user() {
         let mut user_map = HashmapUserStore::default();
         let new_user = User {
-            email: "test_user".to_string(),
-            password: "123123".to_string(),
+            email: Email("test_user".to_string()),
+            password: Password("123123".to_string()),
             requires_2fa: false,
         };
         assert_eq!(
             Ok(()),
-            HashmapUserStore::add_user(&mut user_map, new_user).await
+            HashmapUserStore::add_user(&mut user_map, &new_user).await
         )
     }
 
@@ -82,15 +82,21 @@ mod tests {
     async fn test_get_user() {
         let user_map = HashmapUserStore::default();
 
-        let user_result = user_map.get_user("admin@email.com").await.unwrap();
-        assert_eq!("admin@email.com".to_owned(), user_result.email);
+        let user_result = user_map
+            .get_user(&Email("admin@email.com".to_string()))
+            .await
+            .unwrap();
+        assert_eq!("admin@email.com", user_result.email.as_ref());
     }
 
     #[tokio::test]
     async fn test_validate_user() {
         let user_map = HashmapUserStore::default();
         assert!(user_map
-            .validate_user("admin@email.com", "123123")
+            .validate_user(
+                Email("admin@email.com".to_string()),
+                Password("123123".to_string())
+            )
             .await
             .is_ok());
     }
