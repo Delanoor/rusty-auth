d2736cd06c7049ab24d24407c89ca589baac6aad
hwn0809 <hwn0809@gmail.com>
2024-05-28 10:37:14 +0900

    Add JWT

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,10 +1,12 @@
-use axum::{extract::State, response::IntoResponse, Json};
-use reqwest::StatusCode;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+
+use axum_extra::extract::CookieJar;
 use serde::Deserialize;
 
 use crate::{
     app_state::AppState,
     domain::{email::Email, password::Password, AuthAPIError},
+    utils::auth::generate_auth_cookie,
 };
 
 #[derive(Deserialize)]
@@ -15,22 +17,32 @@ pub struct LoginRequest {
 
 pub async fn login(
     State(state): State<AppState>,
+    jar: CookieJar,
     Json(request): Json<LoginRequest>,
-) -> Result<impl IntoResponse, AuthAPIError> {
-    let email = Email::parse(request.email).map_err(|_| AuthAPIError::InvalidCredentials)?;
-    let password =
-        Password::parse(request.password).map_err(|_| AuthAPIError::InvalidCredentials)?;
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    let email = match Email::parse(request.email) {
+        Ok(email) => email,
+        Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
+    };
+
+    let password = match Password::parse(request.password) {
+        Ok(password) => password,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
+
+    let auth_cookie = generate_auth_cookie(&email).unwrap();
+    let updated_jar = jar.add(auth_cookie);
 
     let user_store = &state.user_store.read().await;
 
     let get_response = user_store.get_user(&email).await;
     if get_response.is_err() {
-        return Err(AuthAPIError::IncorrectCredentials);
+        return (updated_jar, Err(AuthAPIError::IncorrectCredentials));
     }
 
     let validate_response = user_store.validate_user(&email, &password).await;
     match validate_response {
-        Ok(_) => Ok(StatusCode::OK.into_response()),
-        Err(_) => Err(AuthAPIError::IncorrectCredentials),
+        Ok(_) => (updated_jar, Ok(StatusCode::OK.into_response())),
+        Err(_) => (updated_jar, Err(AuthAPIError::IncorrectCredentials)),
     }
 }
