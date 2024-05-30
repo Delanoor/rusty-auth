03edaebd54c5c705e4e73b3f9530ef6a15d84322
hwn0809 <hwn0809@gmail.com>
2024-05-30 13:09:43 +0900

    add banned_token_store

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -2,13 +2,15 @@ use std::sync::Arc;
 
 use auth_service::app_state::AppState;
 use auth_service::services::hashmap_user_store::HashmapUserStore;
+use auth_service::services::hashset_banned_token_store::HashsetBannedTokenStore;
 use auth_service::utils::constants::prod;
 use auth_service::Application;
 
 #[tokio::main]
 async fn main() {
     let user_store = Arc::new(tokio::sync::RwLock::new(HashmapUserStore::default()));
-    let app_state = AppState::new(user_store);
+    let token_store = Arc::new(tokio::sync::RwLock::new(HashsetBannedTokenStore::default()));
+    let app_state = AppState::new(user_store, token_store);
     let app = Application::build(app_state, prod::APP_ADDRESS)
         .await
         .expect("Failed to build application");
