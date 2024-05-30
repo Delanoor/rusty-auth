70d2b69853f0283753cf671dece39f716b95b6c5
hwn0809 <hwn0809@gmail.com>
2024-05-30 10:35:35 +0900

    add verify-token route

--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,10 +1,60 @@
+use auth_service::{
+    domain::email::Email,
+    utils::{auth::generate_auth_cookie, constants::JWT_COOKIE_NAME},
+};
+use reqwest::Url;
+
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn verify_token_returns_status_code() {
+async fn should_return_200_valid_token() {
     let app = TestApp::new().await;
+    let email = Email::parse("test@email.com".to_string()).unwrap();
+    let cookie = generate_auth_cookie(&email).unwrap();
 
-    let response = app.get_verify_token().await;
+    app.cookie_jar.add_cookie_str(
+        &format!(
+            "{}={}; HttpOnly; SameSite=Lax; Secure; Path=/",
+            JWT_COOKIE_NAME,
+            cookie.value()
+        ),
+        &Url::parse(&app.address).expect("Failed to parse URL"),
+    );
+
+    let body = serde_json::json!({
+        "token" : cookie.value()
+    });
+    let response = app.post_verify_token(&body).await;
 
     assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+
+    let body = serde_json::json!({
+        "token": "invalid",
+    });
+    let response = app.post_verify_token(&body).await;
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+#[tokio::test]
+async fn should_return_422_if_malformed_input() {
+    let app = TestApp::new().await;
+
+    let inputs = [
+        serde_json::json!({
+            "email": "test@email.com",
+
+        }),
+        serde_json::json!({}),
+    ];
+
+    for input in inputs.iter() {
+        let response = app.post_verify_token(&input).await;
+
+        assert_eq!(response.status().as_u16(), 422);
+    }
 }
