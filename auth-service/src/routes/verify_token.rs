27891a880f033d999d9492b4d44a6af780a095c6
hwn0809 <hwn0809@gmail.com>
2024-05-30 10:35:35 +0900

    add verify-token route

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,22 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{app_state::AppState, domain::AuthAPIError, utils::auth::validate_token};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct VerifyTokenRequest {
+    token: String,
+}
+
+pub async fn verify_token(
+    State(state): State<AppState>,
+    Json(request): Json<VerifyTokenRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let token = request.token;
+
+    match validate_token(&token).await {
+        Ok(_) => Ok(()),
+        Err(_) => Err(AuthAPIError::InvalidToken),
+    }
 }
